1a48fef81b246b14636a0c1c1e649f1d11cb677d
Belyaev Vadim <a3unnn@gmail.com>
2025-03-05 22:05:12 +0500

    working, but pytest failed

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -34,6 +34,9 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
+    def __str__(self):
+        return self.title
+
 
 class Location(models.Model):
     name = models.CharField(
@@ -53,6 +56,9 @@ class Location(models.Model):
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -99,3 +105,6 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,14 +1,52 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Post, Category
+import datetime as dt
 
 
-class HomePage(TemplateView):
+class HomePage(ListView):
     template_name = "blog/index.html"
 
+    def get_queryset(self):
+        queryset = Post.objects.filter(
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=dt.datetime.now()
+        ).order_by('-pub_date')[:5]
+        return queryset
 
-class PostDetail(TemplateView):
+
+class PostDetail(DetailView):
     template_name = "blog/detail.html"
+    context_object_name = 'post'
+
+    def get_object(self):
+        return get_object_or_404(
+            Post,
+            id=self.kwargs['pk'],
+            is_published=True,
+            category_id__is_published=True,
+            pub_date__lte=dt.datetime.now()
+        )
 
 
-class CategoryPosts(TemplateView):
+class CategoryPosts(ListView):
     template_name = "blog/category.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        category_slug = self.kwargs['pk']
+        context['post_list'] = get_list_or_404(
+            Post,
+            category_id__slug=category_slug,
+            is_published=True
+        )
+        context['category'] = get_object_or_404(
+            Category,
+            slug=category_slug,
+            is_published=True
+        )
+        return context
